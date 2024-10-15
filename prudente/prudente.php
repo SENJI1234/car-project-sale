c5b4b204ab0508c7c1a5ad8e355000c35718b620
SENJI1234 <hakdoghakdog860@gmail.com>
2024-10-15 14:29:27 +0800

    Add files via upload

---
+++ prudente/prudente.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="low.css">
+</head>
+<body>
+
+ <header>
+    <div class="one">
+        <div class="two">
+         <img src="images/p.png" alt="" id="oness">
+         <img src="images/a.png" alt="" id="ones" style="width: 230px;">
+         </div>
+         <h1 style="margin-right: 150px;" id="yows">PRODUCT CARD</h1>
+         <img src="images/car.png" alt="" id="oness">
+        </div>
+ </header>
+
+
+   <div class="three">
+    <div class="four">
+    <img src="images/yellow.png" alt="" id="yellow">
+    <h2>Zenvo ST1 Sports</h2>
+    <button id="red">view details</button>
+    <h3>$120</h3>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur<br> adipisicing elit. Exercitationem, </p>
+    <button id="buy">Buy now</button>
+
+
+
+    
+    </div>
+    <div class="four">
+    <img src="images/bull.png" alt="" id="yellow" style="width: 200px;">
+    <h2>lecia ST1 Sports</h2>
+    <button id="red">view details</button>
+    <h3>$120</h3>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur<br> adipisicing elit. Exercitationem, </p>
+    <button id="buy">Buy now</button>
+
+
+
+    
+    </div>
+    <div class="four">
+    <img src="images/lack.png" alt="" id="yellow">
+    <h2>kanvo ST1 Sports</h2>
+    <button id="red">view details</button>
+    <h3>$120</h3>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur<br> adipisicing elit. Exercitationem, </p>
+    <button id="buy">Buy now</button>
+
+
+
+    
+    </div>
+
+    <div class="four">
+    <img src="images/col.png" alt="" id="yellow">
+    <h2>bugatti ST1 Sports</h2>
+    <button id="red">view details</button>
+    <h3>$120</h3>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur<br> adipisicing elit. Exercitationem, </p>
+    <button id="buy">Buy now</button>
+
+
+
+    
+    </div>
+    <div class="four">
+    <img src="images/tesla.png" alt="" id="yellow" style="width: 210px;">
+    <h2>varon ST1 Sports</h2>
+    <button id="red">view details</button>
+    <h3>$120</h3>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur<br> adipisicing elit. Exercitationem, </p>
+    <button id="buy">Buy now</button>
+
+
+
+    
+    </div>
+    <div class="four">
+    <img src="images/safari.png" alt="" id="yellow">
+    <h2>safari ST1 Sports</h2>
+    <button id="red">view details</button>
+    <h3>$120</h3>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur<br> adipisicing elit. Exercitationem, </p>
+    <button id="buy">Buy now</button>
+
+
+
+    
+    </div>
+    <div class="four">
+    <img src="images/ferari.png" alt="" id="yellow">
+    <h2>ferari ST2 Sports</h2>
+    <button id="red">view details</button>
+    <h3>$120</h3>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur<br> adipisicing elit. Exercitationem, </p>
+    <button id="buy">Buy now</button>
+
+
+
+    
+    </div>
+    <div class="four">
+    <img src="images/lamb.png" alt="" id="yellow">
+    <h2>Zenvo ST2 Sports</h2>
+    <button id="red">view details</button>
+    <h3>$120</h3>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur<br> adipisicing elit. Exercitationem, </p>
+    <button id="buy">Buy now</button>
+
+
+
+    
+    </div>
+    <div class="four">
+    <img src="images/hap.png" alt="" id="yellow" style="width: 240px;">
+    <h2>lamboe ST9 Sports</h2>
+    <button id="red">view details</button>
+    <h3>$120</h3>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur<br> adipisicing elit. Exercitationem, </p>
+    <button id="buy">Buy now</button>
+
+
+
+    
+    </div>
+    <div class="four">
+    <img src="images/bugg.png" alt="" id="yellow" style="width: 260px;">
+    <h2>bugatti T1 Sports</h2>
+    <button id="red">view details</button>
+    <h3>$120</h3>
+    <hr>
+    <p>Lorem ipsum dolor sit amet consectetur<br> adipisicing elit. Exercitationem, </p>
+    <button id="buy">Buy now</button>
+
+
+
+    
+    </div>
+   </div>
+    
+
+
+
+
+ <footer>
+    <div class="LAST">
+    <h1>HOME</h1>
+    <h1>TALK TO ME</h1>
+    <h1>ABOUT</h1>
+    <h1>CONTACT</h1>
+    </div>
+ </footer>
+
+
+
+
+
+
+    
+</body>
+</html>
